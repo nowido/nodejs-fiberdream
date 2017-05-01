0609db0ec83a2a1bf6c327592f9fc856587a6884
nowido <nowido@mail.ru>
2017-05-01 16:07:56 +0300

    Add files via upload

---
+++ dsmaster.js
@@ -0,0 +1,122 @@
+// run this script with <unique token> to start Master for <unique token> processing
+
+//--------------------------------------------------
+
+const token = process.argv[2] ? process.argv[2] : 'PI';
+
+//--------------------------------------------------
+
+const redis = require('redis');
+
+//--------------------------------------------------
+//*
+const redisHost = '127.0.0.1';
+const redisPort = '6379';
+const redisConfig = {host: redisHost, port: redisPort};
+//*/
+/*
+const redisHost = 'redis-12559.c10.us-east-1-2.ec2.cloud.redislabs.com';
+const redisPort = '12559';
+const redisToken = '123456';
+const redisConfig = {host: redisHost, port: redisPort, password: redisToken};
+*/
+
+const jobQueueKey = 'job#' + token;
+const resultsQueueKey = 'results#' + token;
+
+const workItemsCount = 4;
+
+//--------------------------------------------------
+
+const redisClient = redis.createClient(redisConfig);
+
+redisClient.on('error', console.log);
+
+//--------------------------------------------------
+
+console.log(`Master for ${workItemsCount} \'${token}\' items processing`);
+
+pushWorkItems(workItemsCount);
+
+var startTime = Date.now();
+
+var results = [];
+
+gatherResults((result) => 
+{    
+    results.push(result);
+
+    console.log(`result #${result.id}: ${result.value.estimation}`);
+
+    if(results.length === workItemsCount)
+    {
+        var runTime = Math.round((Date.now() - startTime) / 10) / 100;
+
+        console.log(`All ${workItemsCount} items done`);
+
+        showStats(results, runTime);
+        
+        return false;
+    }            
+
+    return true;
+});
+
+//--------------------------------------------------
+
+function pushWorkItems(workItemsCount)
+{
+    for(var i = 0; i < workItemsCount; ++i)
+    {
+        redisClient.lpush(jobQueueKey, JSON.stringify({id: i}));
+    }
+}
+
+//--------------------------------------------------
+
+function gatherResults(onResultItem)
+{        
+    function getResult(err, res)
+    {        
+        if(onResultItem(JSON.parse(res[1])))
+        {
+            redisClient.brpop(resultsQueueKey, 0, getResult);   
+        }
+        else
+        {
+            redisClient.quit();
+            process.exit(0);                            
+        }
+    }    
+
+    redisClient.brpop(resultsQueueKey, 0, getResult);
+}
+
+//--------------------------------------------------
+
+function showStats(results, runTime)
+{    
+    var pointsCount = 0;
+    var average = 0;
+
+    var itemsCount = results.length;
+
+    for(var i = 0; i < itemsCount; ++i)
+    {
+        var item = results[i];
+
+        pointsCount += item.value.count;
+        average += item.value.estimation;
+    }
+        
+    average /= itemsCount;
+
+    var gigaPoints = Math.round(pointsCount / runTime / 1e6) / 1e3;
+        
+    console.log(`Monte-Carlo Pi = ${average} (run time: ${runTime} s, ${gigaPoints} Gpts/s)`);
+}
+
+//--------------------------------------------------
+
+
+
---
+++ dsnode.js
@@ -0,0 +1,145 @@
+// run this script with <unique token> to start node for <unique token> processing
+
+//--------------------------------------------------
+
+const token = process.argv[2] ? process.argv[2] : 'PI';
+
+//--------------------------------------------------
+
+const cluster = require('cluster');
+
+const cpus = require('os').cpus();
+const cpu = cpus[0];
+const numCPUs = 2;//cpus.length;
+
+const pid = process.pid;
+
+//--------------------------------------------------
+
+//*
+const redisHost = '127.0.0.1';
+const redisPort = '6379';
+const redisConfig = {host: redisHost, port: redisPort};
+//*/
+/*
+const redisHost = 'redis-12559.c10.us-east-1-2.ec2.cloud.redislabs.com';
+const redisPort = '12559';
+const redisToken = '123456';
+const redisConfig = {host: redisHost, port: redisPort, password: redisToken};
+*/
+
+const jobQueueKey = 'job#' + token;
+const resultsQueueKey = 'results#' + token;
+
+//--------------------------------------------------
+
+if(cluster.isMaster) 
+{
+    runManager();
+}
+else // if(cluster.isWorker)
+{
+    runWorker();
+}
+
+//--------------------------------------------------
+
+function runManager()
+{
+    console.log(`Active node ${pid} on ${cpu.model}, ${cpu.speed} MHz, ${numCPUs} logical CPUs for \'${token}\' processing`);
+
+    for (var i = 0; i < numCPUs; ++i) 
+    {
+        cluster.fork();
+    } 
+
+    cluster.on('online', (worker) => 
+    {
+        console.log(`worker ${worker.process.pid} is running.`);    
+    });
+
+    cluster.on('exit', (worker) => 
+    {
+        console.log(`worker ${worker.process.pid} exited; forking new one.`);
+
+        cluster.fork();    
+    });   
+
+    cluster.on('message', (worker, message) => 
+    {
+        if(message.error)
+        {
+            console.log(`worker ${worker.process.pid} error: ${message.error}`);    
+        }
+        else if(message.jobResult)
+        {
+            var result = message.jobResult;
+
+            console.log(`job result: id=${result.id}, value=${result.value}`);
+        }
+    });          
+}
+
+//--------------------------------------------------
+
+function runWorker()
+{
+    const redis = require('redis');    
+
+    const redisClient = redis.createClient(redisConfig);
+    
+    redisClient.on('error', (err) => 
+    {
+        process.send({error: err});    
+    });
+
+    function calcMonteCarloPi(pointsCount)
+    {
+        var inCircleCount = 0;
+
+        for (var i = 0; i < pointsCount; ++i)
+        {
+            var x = Math.random();
+            var y = Math.random();
+
+            if(x * x + y * y < 1)
+            {
+                ++inCircleCount;
+            }
+        }
+
+        return 4 * inCircleCount / pointsCount;        
+    }
+
+    function doWork(jobItem)
+    {
+        const N = 100000000; // 100 millions of random test points
+
+        return {            
+            id: jobItem.id, 
+            value: {count: N, estimation: calcMonteCarloPi(N)}
+        };
+    }
+
+    function getJob(err, res)
+    {
+        var job = JSON.parse(res[1]);
+
+        var result = doWork(job);
+
+        console.log(`${result.value.estimation}`);
+
+        process.send
+        ({
+            jobResult: {id: result.id, value: result.value.estimation}
+        });            
+            
+        redisClient.lpush(resultsQueueKey, JSON.stringify(result));
+
+        redisClient.brpop(jobQueueKey, 0, getJob);   
+    }
+
+    redisClient.brpop(jobQueueKey, 0, getJob);    
+}
+
+//--------------------------------------------------
